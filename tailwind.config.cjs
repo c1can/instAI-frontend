63cdcb7451178f3807d7d43e28781df281c99893
carlosreyes <carlosreyes8436@gmail.com>
2023-03-04 18:54:03 -0600

    add header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.jsx"
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem'
+      }
+    },
     extend: {},
   },
   plugins: [],
