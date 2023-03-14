b48d6687e30439150f4ec5eb7ae7a61d2c85d9fe
carlosreyes <carlosreyes8436@gmail.com>
2023-03-14 15:28:12 -0600

    change prompt color

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -34,7 +34,7 @@ export function Home() {
                                     <div className="Card" key={_id}>
                                         <img src={image} alt={prompt} />
                                         <div className="info px-2 py-4 flex flex-col gap-4 bg-black opacity-90">
-                                            <p className="text-gray-4000"><span className="font-bold text-white">Prompt: </span>{prompt}</p>
+                                            <p className="text-gray-400"><span className="font-bold text-white">Prompt: </span>{prompt}</p>
                                             <p className="text-gray-400"><span className="font-bold text-white">Created by: </span>{user}</p>
                                         </div>
                                     </div>
