9c0cd5a9222fa6b26c70240c3b7e4d9c3c3edf09
carlosreyes <carlosreyes8436@gmail.com>
2023-08-14 13:05:46 -0600

    change card stlye

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -2,10 +2,10 @@ function Card({ user, prompt, image, _id, avatar }) {
     return (
         <div className="Card" key={_id}>
             <img src={image} alt={prompt} />
-            <div className="info p-6 flex flex-col gap-4 bg-black opacity-90">
+            <div className="info p-6 flex flex-col gap-4 bg-black opacity-90 rounded-sm">
                 <p className="text-gray-400"><span className="font-bold text-white">Prompt: </span>{prompt}</p>
                 <div className="user_data flex items-center justify-between">
-                   <p className="text-gray-400"><span className="font-bold text-white">Created by: </span>{user}</p>
+                   <p className="text-gray-400"><span className="font-bold text-white">Shared by: </span>{user}</p>
                     <div style={{backgroundImage: `${avatar ? `url(${avatar})` : null}`}} className={`avatar border border-gray-100 h-[35px] w-[35px] border-opacity-50 rounded-full ${avatar ? 'bg-cover bg-center bg-no-repeat' : 'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'}`}></div>
                     </div>
                 </div>
