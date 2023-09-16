121ada54951cb114396d0d44f7988e76c132c42d
carlosreyes <carlosreyes8436@gmail.com>
2023-09-16 17:19:56 -0600

    add optional chaining to user object to prevent errors

--- src/hooks/useMetadata.js
+++ src/hooks/useMetadata.js
@@ -3,12 +3,13 @@ import { useAuth } from "./auth";
 export function useMetadata() {
     const { user } = useAuth()
 
+   //{session: null}  ---> when logout
 
-    const metadata = user.session.user.user_metadata
-    const userAvatar = metadata.avatar_url
-    const username = metadata.user_name
-    const fullName = metadata.full_name
-    const userEmail = user.session.user.email
+    const metadata = user.session?.user.user_metadata
+    const userAvatar = metadata?.avatar_url
+    const username = metadata?.user_name
+    const fullName = metadata?.full_name
+    const userEmail = user.session?.user.email
 
     return { userAvatar, username, fullName, userEmail } 
 }
